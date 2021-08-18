f6ab478f600096632e52fb1768425341691a07b3
Faramarz Bodaghi <Faramarz.Bodaghi@dnv.com>
2021-08-18 13:07:05 +0200

    Some changes

---
+++ MyPlayGround/TestDMClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using DNVS.NPS.DM.ServiceFacade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDMClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDMServerToServerService service = new DMServerToServerService();
+            var test = service.FindTechdoc(new DNVS.NPS.DM.ServiceFacade.FindModels.TechdocFindModel() { FolderName = "D34893" });
+        }
+    }
+}
